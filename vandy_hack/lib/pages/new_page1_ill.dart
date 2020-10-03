4fca4cf0fffa66f597292cff5f7bbf3baaf504c6
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2020-10-03 22:01:10 +0700

    add state picture and change page theme

--- vandy_hack/lib/pages/new_page1_ill.dart
+++ vandy_hack/lib/pages/new_page1_ill.dart
@@ -58,12 +58,10 @@ class ill extends StatelessWidget {
     Widget textSection = Container(
       padding: const EdgeInsets.all(32),
       child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
+        'Description box'
+            '               '
+            '               '
+            '               ',
         softWrap: true,
       ),
     );
@@ -86,6 +84,44 @@ class ill extends StatelessWidget {
             titleSection,
             buttonSection,
             textSection,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Image.asset('img/ill.png'),
+                    ),
+                    Expanded(
+                      child: Image.asset('img/ill.png'),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Image.asset('img/ill.png'),
+                    ),
+                    Expanded(
+                      child: Image.asset('img/ill.png'),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Image.asset('img/ill.png'),
+                    ),
+                    Expanded(
+                      child: Image.asset('img/ill.png'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
